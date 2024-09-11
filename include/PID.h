049a5d581168542b430883f528c6b2191c0622e2
RedBoxMan <Samsleake@gmail.com>
2024-09-11 17:12:54 -0500

    Added and Setup files
    Basic PID still needs ODOM implementation

--- include/PID.h
+++ include/PID.h
@@ -2,7 +2,17 @@
 
 #include "vex.h"
 
+//PID Class
 class PID
 {
-    
+    float Kp, Ki, Kd;
+
+    float integral, derivative;
+
+    float prevError;
+
+    PID();
+
+    float compute(float error);
+
 };
--- src/PID.cpp
+++ src/PID.cpp
@@ -1,2 +1,19 @@
 #include "PID.h"
 
+PID::PID(){}
+
+
+float PID::compute(float error)
+{
+    integral = integral + error;
+
+    if(error <= 0)
+        integral = 0;
+
+    derivative = error - prevError;
+    prevError = error;
+
+    float output = error*Kp + integral*Ki + derivative*Kd;
+
+    return output;
+}
